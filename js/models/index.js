422cf50779fbc7b0e7b5910a8f17bacaa59ba89b
Ramūns Usovs <ramuuns@enkurs.org>
2013-09-30 20:32:52 +0300

    Typos

--- js/models/index.js
+++ js/models/index.js
@@ -4,7 +4,7 @@ CV.IndexModel.reopenClass({
 	createCVData : function(source) {
 		var obj = {};
 		source.models.forEach(function(item,index){
-			obj[item] = CV.[item.substr(0,1).toUpperCase() + item.substr(1) + 'Model'].create(source[item]);
+			obj[item] = CV[item.substr(0,1).toUpperCase() + item.substr(1) + 'Model'].create(source[item]);
 		});
 		return CV.IndexModel.create(obj);
 	}
