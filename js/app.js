a047b23d2eaf5cd2aef93bdda7eb91981b8802fc
Ramūns Usovs <ramuuns@enkurs.org>
2013-10-01 15:12:16 +0300

    made it pretty

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement("style");
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      "@-ms-viewport{width:auto!important}"
+    )
+  );
+  document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+}
+
 CV = Em.Application.create({});
 
 CV.Router.reopen({
